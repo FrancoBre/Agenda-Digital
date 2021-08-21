04aab439b428bcb99aede750a1f0349aecda77b4
FrancoBre <bregolif.fb@gmail.com>
2021-08-21 19:07:31 -0300

    Se agregaron tablas

--- agenda/sql/scriptAgenda.sql
+++ agenda/sql/scriptAgenda.sql
@@ -6,12 +6,29 @@ idTipo_contacto int(11) NOT NULL AUTO_INCREMENT,
 Tipo enum('Amigo', 'Familiar', 'Trabajo', 'Futbol'), 
 PRIMARY KEY (idTipo_contacto)); 
 
+CREATE TABLE provincia (
+idProvincia int(11) NOT NULL AUTO_INCREMENT,
+Nombre varchar(45) NOT NULL,
+Pais varchar(45) NOT NULL,
+PRIMARY KEY (idProvincia), 
+CONSTRAINT fk_pais FOREIGN KEY (Pais) REFERENCES pais(idPais)
+);
+
+CREATE TABLE pais (
+idPais int(11) NOT NULL AUTO_INCREMENT,
+Nombre varchar(45) NOT NULL,
+PRIMARY KEY (idPais)
+);
+
 CREATE TABLE localidad (
 idLocalidad int(11) NOT NULL AUTO_INCREMENT,
 Nombre varchar(45) NOT NULL,
 Provincia varchar(45),
 Pais varchar(45), 
-PRIMARY KEY (idLocalidad));
+PRIMARY KEY (idLocalidad), 
+CONSTRAINT fk_pais FOREIGN KEY (Pais) REFERENCES pais(idPais), 
+CONSTRAINT fk_provincia FOREIGN KEY (Provincia) REFERENCES provincia(idProvincia)
+);
 
 CREATE TABLE domicilio (
 idDomicilio int(11) NOT NULL AUTO_INCREMENT, 
@@ -21,7 +38,8 @@ Piso int(20),
 Depto int(20),
 Localidad int(11),
 PRIMARY KEY (idDomicilio), 
-CONSTRAINT fk_localidad FOREIGN KEY (Localidad) REFERENCES localidad(idLocalidad));
+CONSTRAINT fk_localidad FOREIGN KEY (Localidad) REFERENCES localidad(idLocalidad)
+);
 
 CREATE TABLE personas (
 idPersona int(11) NOT NULL AUTO_INCREMENT, 
@@ -32,6 +50,7 @@ Tipo_contacto int(11),
 PRIMARY KEY (idPersona), 
 CONSTRAINT fk_domicilio FOREIGN KEY (Domicilio) REFERENCES domicilio(idDomicilio), 
 CONSTRAINT fk_tipo FOREIGN KEY (Tipo_contacto) REFERENCES tipo_contacto(idTipo_contacto));
+
 
 
 
--- agenda/src/main/java/persistencia/conexion/Conexion.java
+++ agenda/src/main/java/persistencia/conexion/Conexion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package persistencia.conexion;
 
 import java.sql.*;
@@ -57,6 +56,7 @@ public class Conexion {
     		}
     	} catch (SQLException se) {
     		se.printStackTrace();
+    		log.error("Error al cerrar la conexion");
     	}
     	
     	log.info("Conexion cerrada");
@@ -76,62 +76,3 @@ public class Conexion {
 		return this.conn;
 	}
 }
-=======
-package persistencia.conexion;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import org.apache.log4j.Logger;
-
-public class Conexion 
-{
-	public static Conexion instancia;
-	private Connection connection;
-	private Logger log = Logger.getLogger(Conexion.class);	
-	
-	private Conexion()
-	{
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver"); // quitar si no es necesario
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda","root","root");
-			this.connection.setAutoCommit(false);
-			log.info("Conexion exitosa");
-		}
-		catch(Exception e)
-		{
-			log.error("Conexion fallida", e);
-		}
-	}
-	
-	
-	public static Conexion getConexion()   
-	{								
-		if(instancia == null)
-		{
-			instancia = new Conexion();
-		}
-		return instancia;
-	}
-
-	public Connection getSQLConexion() 
-	{
-		return this.connection;
-	}
-	
-	public void cerrarConexion()
-	{
-		try 
-		{
-			this.connection.close();
-			log.info("Conexion cerrada");
-		}
-		catch (SQLException e) 
-		{
-			log.error("Error al cerrar la conexion!", e);
-		}
-		instancia = null;
-	}
-}
->>>>>>> branch 'master' of https://github.com/FrancoBre/Agenda-Digital
