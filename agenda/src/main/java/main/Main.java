69598ff351c7ad4d3bfa209fa04e2600ef95e395
FrancoBre <bregolif.fb@gmail.com>
2021-08-27 20:11:46 -0300

    Ahora sí merge furioso

--- agenda/src/main/java/main/Main.java
+++ agenda/src/main/java/main/Main.java
@@ -4,7 +4,6 @@ import modelo.Agenda;
 import persistencia.dao.mysql.DAOSQLFactory;
 import presentacion.controlador.*;
 import presentacion.vista.Vista;
-<<<<<<< HEAD
 
 public class Main {
     public static void main(String[] args) {
@@ -19,23 +18,4 @@ public class Main {
 	controladorEditar.inicializar();
 	controladorTipo.inicializar();
     }
-
-=======
-
-public class Main 
-{
-	public static void main(String[] args) 
-	{
-		Vista vista = new Vista();
-		Agenda modelo = new Agenda(new DAOSQLFactory());
-		ControladorVista controladorVista = new ControladorVista(vista, modelo);
-		ControladorAgregar controladorAgregar = new ControladorAgregar(vista,modelo);
-		ControladorEditar controladorEditar = new ControladorEditar(vista,modelo);
-		ControladorABM_Tipo controladorTipo = new ControladorABM_Tipo(vista, modelo);
-		controladorVista.inicializar();
-		controladorAgregar.inicializar();
-		controladorEditar.inicializar();
-		controladorTipo.inicializar();
-	}
->>>>>>> branch 'master' of https://github.com/FrancoBre/Agenda-Digital
 }
