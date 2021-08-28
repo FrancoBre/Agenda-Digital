c20a0a7f8579c18470c1c79406e7d40b0266e71f
nluna <nluna190898@gmail.com>
2021-08-28 17:04:10 -0300

    prueba

--- agenda/src/main/java/persistencia/dao/mysql/PersonaDAOSQL.java
+++ agenda/src/main/java/persistencia/dao/mysql/PersonaDAOSQL.java
@@ -17,7 +17,7 @@ import dto.PersonaDTO;
 
 public class PersonaDAOSQL implements PersonaDAO {
 
-    private static final String insert = "INSERT INTO personas(idPersona, nombre, telefono, email, nacimiento, domicilio, tipo_contacto) VALUES(?, ?, ?, ?, ?, ?, ?)";
+    private static final String insert = " INSERT INTO personas(idPersona, nombre, telefono, email, nacimiento, domicilio, tipo_contacto) VALUES(?, ?, ?, ?, ?, ?, ?)";
     private static final String update = "UPDATE personas SET nombre = ? , telefono = ? , email = ? , nacimiento = ? , domicilio = ?, tipo_contacto = ? WHERE idPersona = ?;";
     private static final String delete = "DELETE FROM personas WHERE idPersona = ?";
     private static final String readall = "SELECT * FROM personas";
